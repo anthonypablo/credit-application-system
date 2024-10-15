f905145ede00231c6157fcd02ca3df4979456dfe
anthonypablo <anthonypablo.dev@gmail.com>
2024-10-15 11:21:25 -0300

    código

--- src/test/kotlin/me/dio/credit_application_system/controller/CreditResourceTest.kt
+++ src/test/kotlin/me/dio/credit_application_system/controller/CreditResourceTest.kt
@@ -2,14 +2,13 @@ package me.dio.credit_application_system.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import me.dio.credit_application_system.dto.CreditDto
-import me.dio.credit_application_system.dto.CustomerDto
-import me.dio.credit_application_system.dto.CustomerUpdateDto
+import me.dio.credit_application_system.entity.Address
 import me.dio.credit_application_system.entity.Customer
 import me.dio.credit_application_system.repository.CustomerRepository
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
@@ -21,7 +20,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import java.math.BigDecimal
 import java.time.LocalDate
-import java.util.*
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -30,22 +28,23 @@ import java.util.*
 class CreditResourceTest {
     @Autowired
     private lateinit var customerRepository: CustomerRepository
-
+    @Autowired
+    lateinit var testEntityManager: TestEntityManager
     @Autowired
     private lateinit var mockMvc: MockMvc
-
     @Autowired
     private lateinit var objectMapper: ObjectMapper
+
+    private lateinit var customer: Customer
 
     companion object {
         const val URL: String = "/api/credits"
     }
 
     @BeforeEach
-    fun setup() = customerRepository.deleteAll()
-
-    @AfterEach
-    fun tarDown() = customerRepository.deleteAll()
+    fun setup() {
+        customer = testEntityManager.persist(buildCustomer())
+    }
 
     @Test
     fun `should create a credit and return 201 status`() {
@@ -67,232 +66,38 @@ class CreditResourceTest {
             .andDo(MockMvcResultHandlers.print())
     }
 
-    @Test
-    fun `should create `() {
-    }
-    /* criar um crédito
-
-    fun toEntity(): Credit = Credit(
-        creditValue = this.creditValue,
-        dayFirstInstallment = this.dayFirstOfInstallment,
-        numberOfInstallments = this.numberOfInstallments,
-        customer = Customer(id = this.customerId)
-
-
-    não pode ter parcela no passado e deve ser no máximo após 3 meses da data atual
-
-    não pode ter mais de  48 parcelas
-
-
-    */
-
-    @Test
-    fun `should not save a customer with same  CPF and return 409 status`() {
-        //given
-        customerRepository.save(builderCustomerDto().toEntity())
-        val customerDto: CustomerDto = builderCustomerDto()
-        val valueAsString: String = objectMapper.writeValueAsString(customerDto)
-        //when
-        //then
-        mockMvc.perform(
-            MockMvcRequestBuilders.post(URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(valueAsString)
-        )
-            .andExpect(MockMvcResultMatchers.status().isConflict)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Conflict! Consult the documentation"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("409"))
-            .andExpect(
-                MockMvcResultMatchers.jsonPath("$.exception")
-                    .value("class org.springframework.dao.DataIntegrityViolationException")
-            )
-            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
-            .andDo(MockMvcResultHandlers.print())
-    }
-
-    @Test
-    fun `should not save a customer with firstName empty and return 400 status`() {
-        //given
-        val customerDto: CustomerDto = builderCustomerDto(firstName = "")
-        val valueAsString: String = objectMapper.writeValueAsString(customerDto)
-        //when
-        //then
-        mockMvc.perform(
-            MockMvcRequestBuilders.post(URL)
-                .content(valueAsString)
-                .contentType(MediaType.APPLICATION_JSON)
-        )
-            .andExpect(MockMvcResultMatchers.status().isBadRequest)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("400"))
-            .andExpect(
-                MockMvcResultMatchers.jsonPath("$.exception")
-                    .value("class org.springframework.web.bind.MethodArgumentNotValidException")
-            )
-            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
-            .andDo(MockMvcResultHandlers.print())
-    }
-
-    @Test
-    fun `should find customer by id and return 200 status`() {
-        //given
-        val customer: Customer = customerRepository.save(builderCustomerDto().toEntity())
-        //when
-        //then
-        mockMvc.perform(
-            MockMvcRequestBuilders.get("$URL/${customer.id}")
-                .accept(MediaType.APPLICATION_JSON)
-        )
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Anthony"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Pablo"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value("584.580.311-08"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("anthony@email.com"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.income").value("1000.0"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("00.000-000"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua Fulano de Tal"))
-            //Funciona apenas individualmente
-            //    .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("1"))
-            .andDo(MockMvcResultHandlers.print())
-    }
-
-    @Test
-    fun `should not find customer with invalid id and return 400 status `() {
-        //given
-        val invalidId: Long = 2L
-        //when
-        //then
-        mockMvc.perform(
-            MockMvcRequestBuilders.get("$URL/$invalidId")
-                .accept(MediaType.APPLICATION_JSON)
-        )
-            .andExpect(MockMvcResultMatchers.status().isBadRequest)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("400"))
-            .andExpect(
-                MockMvcResultMatchers.jsonPath("$.exception")
-                    .value("class me.dio.credit_application_system.exception.BusinessException")
-            )
-            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
-            .andDo(MockMvcResultHandlers.print())
-    }
-
-    @Test
-    fun `should delete customer by id and return 204 status`() {
-        //given
-        val customer: Customer = customerRepository.save(builderCustomerDto().toEntity())
-        //when
-        mockMvc.perform(
-            MockMvcRequestBuilders.delete("$URL/${customer.id}")
-                .accept(MediaType.APPLICATION_JSON)
-        )
-            .andExpect(MockMvcResultMatchers.status().isNoContent)
-            .andDo(MockMvcResultHandlers.print())
-        //then
-    }
-
-    @Test
-    fun `should not delete customer by id and return 400 status`() {
-        //given
-        val invalidId: Long = Random().nextLong()
-        //when
-        mockMvc.perform(
-            MockMvcRequestBuilders.delete("$URL/$invalidId")
-                .accept(MediaType.APPLICATION_JSON)
-        )
-            .andExpect(MockMvcResultMatchers.status().isBadRequest)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("400"))
-            .andExpect(
-                MockMvcResultMatchers.jsonPath("$.exception")
-                    .value("class me.dio.credit_application_system.exception.BusinessException")
-            )
-            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
-            .andDo(MockMvcResultHandlers.print())
-        //then
-    }
-
-    @Test
-    fun `should update a customer and return 200 status`() {
-        //given
-        val customer: Customer = customerRepository.save(builderCustomerDto().toEntity())
-        val customerUpdateDto: CustomerUpdateDto = builderCustomerUpdateDto()
-        val valueAsString: String = objectMapper.writeValueAsString(customerUpdateDto)
-        //when
-        //then
-        mockMvc.perform(
-            MockMvcRequestBuilders.patch("$URL?customerId=${customer.id}")
-                .content(valueAsString)
-                .contentType(MediaType.APPLICATION_JSON)
-        )
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Maria"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("da Silva"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value("584.580.311-08"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("anthony@email.com"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.income").value("5000.0"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("01.001-001"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua Updated"))
-            //funciona apenas individualmente
-            // .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("1"))
-            .andDo(MockMvcResultHandlers.print())
-    }
-
-    @Test
-    fun `should not update a customer with invalid id and return 400 status`() {
-        //given
-        val invalidId: Long = Random().nextLong()
-        val customerUpdateDto: CustomerUpdateDto = builderCustomerUpdateDto()
-        val valueAsString: String = objectMapper.writeValueAsString(customerUpdateDto)
-        //when
-        //then
-        mockMvc.perform(
-            MockMvcRequestBuilders.patch("$URL?customerId=$invalidId")
-                .content(valueAsString)
-                .contentType(MediaType.APPLICATION_JSON)
-        )
-            .andExpect(MockMvcResultMatchers.status().isBadRequest)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("400"))
-            .andExpect(
-                MockMvcResultMatchers.jsonPath("$.exception")
-                    .value("class me.dio.credit_application_system.exception.BusinessException")
-            )
-            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
-            .andDo(MockMvcResultHandlers.print())
-
-    }
 
     private fun builderCreditDto(
         creditValue: BigDecimal = BigDecimal.valueOf(500.0),
         numberOfInstallments: Int = 3,
-        dayFirstOfInstallment: LocalDate = LocalDate.of(2024,11,15),
-        customerId: Long? = null
+        dayFirstOfInstallment: LocalDate = LocalDate.of(2024, 11, 15),
+        customerId: Long = 1
     ) = CreditDto(
         creditValue = creditValue,
         numberOfInstallments = numberOfInstallments,
         dayFirstOfInstallment = dayFirstOfInstallment,
         customerId = customerId
     )
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    private fun builderCustomerUpdateDto(
-        firstName: String = "Maria",
-        lastName: String = "da Silva",
-        income: BigDecimal = BigDecimal.valueOf(5000.00),
-        zipCode: String = "01.001-001",
-        street: String = "Rua Updated"
-    ) = CustomerUpdateDto(
+    private fun buildCustomer(
+        firstName: String = "Anthony",
+        lastName: String = "Pablo",
+        cpf: String = "584.580.311-08",
+        email: String = "anthony@email.com",
+        password: String = "123456",
+        zipCode: String = "00.000-000",
+        street: String = "Rua Fulano de Tal",
+        income: BigDecimal = BigDecimal.valueOf(1000.00),
+    ) = Customer(
         firstName = firstName,
         lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street
+        ),
         income = income,
-        zipCode = zipCode,
-        street = street
     )
-}
 }
--- src/test/kotlin/me/dio/credit_application_system/service/CreditServiceTest.kt
+++ src/test/kotlin/me/dio/credit_application_system/service/CreditServiceTest.kt
@@ -1,4 +1,75 @@
 package me.dio.credit_application_system.service
 
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.verify
+import me.dio.credit_application_system.dto.CreditDto
+import me.dio.credit_application_system.entity.Address
+import me.dio.credit_application_system.entity.Credit
+import me.dio.credit_application_system.entity.Customer
+import me.dio.credit_application_system.exception.BusinessException
+import me.dio.credit_application_system.repository.CreditRepository
+import me.dio.credit_application_system.service.impl.CreditService
+import me.dio.credit_application_system.service.impl.CustomerService
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.*
+
+@ExtendWith(MockKExtension::class)
 class CreditServiceTest {
+
+    @MockK
+    lateinit var creditRepository: CreditRepository
+
+    @InjectMockKs
+    lateinit var creditService: CreditService
+
+    @Test
+    fun `should create credit`() {
+        //given
+        val fakeCredit: Credit = buildCredit()
+        every { creditRepository.save(any()) } returns fakeCredit
+        //when
+        val actual: Credit = creditService.save(fakeCredit)
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isSameAs(fakeCredit)
+        verify(exactly = 1) { creditRepository.save(fakeCredit) }
+    }
+
+    private fun buildCredit(
+        id: Long? = null,
+        creditValue: BigDecimal = BigDecimal.valueOf(500.0),
+        numberOfInstallments: Int = 3,
+        dayFirstOfInstallment: LocalDate = LocalDate.of(2024, 11, 15),
+        customerId: Long = 1
+    ) = Credit(
+        id = id,
+        creditValue = creditValue,
+        numberOfInstallments = numberOfInstallments,
+        dayFirstOfInstallment = dayFirstOfInstallment,
+        customerId = customerId
+    )
+
+
+    @Test
+    fun `should find credit by id`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        val fakeCredit: Credit = buildCredit(id = fakeId)
+        every { creditRepository.findById(fakeId) } returns Optional.of(fakeCredit)
+        //when
+        val actual: Credit? = creditService.findById(fakeId)
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isExactlyInstanceOf(Credit::class.java)
+        Assertions.assertThat(actual).isEqualTo(fakeCredit)
+        verify(exactly = 1) { creditRepository.findById(fakeId) }
+    }
 }
